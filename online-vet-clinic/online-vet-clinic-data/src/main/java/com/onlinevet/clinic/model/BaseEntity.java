52088d9354d09d9b382e7f4ba5592ec33b4695c2
Chirag21 <cp.chirag21@gmail.com>
2021-03-25 10:28:33 +0530

    Annotated BaseEntity as MappedSuperClass.

--- online-vet-clinic/online-vet-clinic-data/src/main/java/com/onlinevet/clinic/model/BaseEntity.java
+++ online-vet-clinic/online-vet-clinic-data/src/main/java/com/onlinevet/clinic/model/BaseEntity.java
@@ -2,11 +2,20 @@ package com.onlinevet.clinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -959454419632272600L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
