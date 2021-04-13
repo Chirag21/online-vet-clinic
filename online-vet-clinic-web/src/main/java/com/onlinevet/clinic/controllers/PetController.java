489dfce5d447dd553d3642737bb6ba794906f7b2
Chirag21 <cp.chirag21@gmail.com>
2021-04-13 14:27:40 +0530

    Partially implemented pet controller.

--- online-vet-clinic-web/src/main/java/com/onlinevet/clinic/controllers/PetController.java
+++ online-vet-clinic-web/src/main/java/com/onlinevet/clinic/controllers/PetController.java
@@ -1,19 +1,55 @@
 package com.onlinevet.clinic.controllers;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.onlinevet.clinic.model.Owner;
+import com.onlinevet.clinic.model.PetType;
+import com.onlinevet.clinic.services.OwnerService;
 import com.onlinevet.clinic.services.PetService;
+import com.onlinevet.clinic.services.PetTypeService;
 
-@RequestMapping("/pets")
+import lombok.AllArgsConstructor;
+
+
 @Controller
+@AllArgsConstructor
+@RequestMapping("/owners/{ownerId}")
 public class PetController {
+	private static final String CREATE_OR_UPDATE_PET_FORM="pets/createOrUpdatePetForm";
+	
+	@Autowired
 	private final PetService petService;
-	public PetController(PetService petService) {
-		super();
-		this.petService = petService;
+	
+	@Autowired
+	private OwnerService ownerService;
+
+	@Autowired
+	private PetTypeService petTypeService;
+	
+	@InitBinder("owner")
+	public void initOwnerBinder(WebDataBinder webDataBinder) {
+		webDataBinder.setDisallowedFields("id");
 	}
+	
+	@ModelAttribute(name = "types")
+	public Set<PetType> populatePetTypes(){
+		return petTypeService.findAll();
+		}
+	
+	@ModelAttribute(name = "owner")
+	public Owner findOwner(@PathVariable("ownerId") Long ownerId) {
+		return ownerService.findById(ownerId);
+	}
+	
 	@RequestMapping({ "", "/", "/pets", "petList", "petList.html" })
 	public String listPets(Model model) {
 		model.addAttribute("pets", petService.findAll());
